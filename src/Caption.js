283157514b585112e47955255a73b22b02a4e6b4
Julius Breckel <julius.breckel@signavio.com>
2017-05-16 13:24:27 +0200

    use prop-types for PropTypes instead of react

--- src/Caption.js
+++ src/Caption.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import DayPickerPropTypes from './PropTypes';
 
 export default function Caption({ date, months, locale, localeUtils, onClick, className, style }) {
